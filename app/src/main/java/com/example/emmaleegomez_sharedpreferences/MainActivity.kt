bd68e0c246b69cb3921ddfed1aedde52f4cb1666
Emmalee Gomez <gomeze@bu.edu>
2024-03-21 17:37:18 -0400

    complete shared preferences

---
+++ app/src/main/java/com/example/emmaleegomez_sharedpreferences/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.emmaleegomez_sharedpreferences
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var changeImage: Button
+    private lateinit var randomImage: ImageView
+    private lateinit var imageTitle: EditText
+    private lateinit var preferencesRepository: PreferencesRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        PreferencesRepository.initialize(this)
+        setContentView(R.layout.activity_main)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        preferencesRepository = PreferencesRepository.get()
+
+        changeImage = findViewById(R.id.change_image)
+        randomImage = findViewById(R.id.random_image)
+        imageTitle = findViewById(R.id.image_title)
+
+        val clickListener = View.OnClickListener { view ->
+            when (view.id) {
+                R.id.change_image -> {
+                    updateImage()
+                }
+            }
+        }
+
+        changeImage.setOnClickListener(clickListener)
+
+        val titleListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                updateTitle()
+            }
+        }
+
+        imageTitle.onFocusChangeListener = titleListener
+
+        lifecycleScope.launch {
+            preferencesRepository.storedImageId.collect { resourceId ->
+                randomImage.setImageResource(resourceId)
+            }
+        }
+
+        lifecycleScope.launch {
+            preferencesRepository.storedImageTitle.collect { title ->
+                imageTitle.setText(title)
+            }
+        }
+    }
+
+    private fun updateImage() {
+        val imageList = arrayOf(
+            R.drawable.cero,
+            R.drawable.uno,
+            R.drawable.dos,
+            R.drawable.tres,
+            R.drawable.cuatro,
+            R.drawable.cinco,
+            R.drawable.seis,
+            R.drawable.siete,
+            R.drawable.ocho,
+            R.drawable.nueve
+        )
+        val randomNum = Random.nextInt(imageList.size)
+        val newRandomImage = imageList[randomNum]
+        val title = imageTitle.text.toString()
+        randomImage.setImageResource(imageList[randomNum])
+
+        lifecycleScope.launch {
+            preferencesRepository.setStoredImageId(newRandomImage)
+            preferencesRepository.setStoredImageTitle(title)
+        }
+    }
+
+    private fun updateTitle() {
+        val title = imageTitle.text.toString()
+        lifecycleScope.launch {
+            preferencesRepository.setStoredImageTitle(title)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/emmaleegomez_sharedpreferences/PreferencesRepository.kt
@@ -0,0 +1,59 @@
+package com.example.emmaleegomez_sharedpreferences
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStoreFile
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+
+class PreferencesRepository(
+    private val dataStore: DataStore<Preferences>
+) {
+    val storedImageId: Flow<Int> = dataStore.data.map {
+        it[IMAGE_ID_KEY] ?: R.drawable.cero
+    }.distinctUntilChanged()
+
+    val storedImageTitle: Flow<String> = dataStore.data.map {
+        it[IMAGE_TITLE_KEY] ?: ""
+    }.distinctUntilChanged()
+
+    suspend fun setStoredImageId(imageId: Int) {
+        dataStore.edit {
+            it[IMAGE_ID_KEY] = imageId
+        }
+    }
+
+    suspend fun setStoredImageTitle(title: String) {
+        dataStore.edit {
+            it[IMAGE_TITLE_KEY] = title
+        }
+    }
+
+    companion object {
+        private val IMAGE_ID_KEY = intPreferencesKey("image_id")
+        private val IMAGE_TITLE_KEY = stringPreferencesKey("image_title")
+        private var INSTANCE: PreferencesRepository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                val dataStore = PreferenceDataStoreFactory.create {
+                    context.preferencesDataStoreFile("settings")
+                }
+
+                INSTANCE = PreferencesRepository(dataStore)
+            }
+        }
+
+        fun get(): PreferencesRepository {
+            return INSTANCE ?: throw IllegalStateException(
+                "PreferencesRepository must be initialized"
+            )
+        }
+    }
+}
